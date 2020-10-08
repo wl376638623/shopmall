77e004184d188431b16fc54d040c6d97d4e1e5d2
wanglu <376638623@qq.com>
2020-10-08 15:32:41 +0900

    'Rcommend'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,27 +1,24 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
-    <swiper>
-      <swiper-item v-for="item in banners">
-        <a :href="item.link">
-          <img :src="item.image" alt="">
-        </a>
-      </swiper-item>
-    </swiper>
+    <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
   </div>
 </template>
 
 <script>
   import NavBar from "components/common/navbar/NavBar";
+  import HomeSwiper from "./childComps/HomeSwiper";
+  import RecommendView from "./childComps/RecommendView";
   import {getHomeMutidata} from "network/home";
-  import {Swiper,SwiperItem} from "components/common/swiper/index"
+
 
   export default {
       name: "Home",
       components: {
           NavBar,
-        Swiper,
-        SwiperItem
+        HomeSwiper,
+        RecommendView
         },
     data(){
         return {
