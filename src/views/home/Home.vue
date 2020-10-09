2a5d8d6b932557349a1b8996d982feec805bf109
wanglu <376638623@qq.com>
2020-10-09 14:29:22 +0900

    '固定栏的添加'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,6 +3,54 @@
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
     <home-swiper :banners="banners"></home-swiper>
     <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
+    <tab-control class="tab-control" :titles="['流行','新款','精选']"></tab-control>
+
+    <ul>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+      <li>11</li>
+    </ul>
+
   </div>
 </template>
 
@@ -10,6 +58,10 @@
   import NavBar from "components/common/navbar/NavBar";
   import HomeSwiper from "./childComps/HomeSwiper";
   import RecommendView from "./childComps/RecommendView";
+
+  import FeatureView from "./childComps/FeatureView";
+  import TabControl from "components/content/tabControl/TabControl";
+
   import {getHomeMutidata} from "network/home";
 
 
@@ -18,7 +70,9 @@
       components: {
           NavBar,
         HomeSwiper,
-        RecommendView
+        RecommendView,
+        FeatureView,
+        TabControl
         },
     data(){
         return {
@@ -38,8 +92,20 @@
 </script>
 
 <style scoped>
+  #home {
+   padding-top: 44px;
+  }
   .home-nav {
     background-color: var(--color-tint);
     color: #fff;
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
+  }
+  .tab-control {
+    position: sticky;
+    top: 44px;
   }
 </style>
