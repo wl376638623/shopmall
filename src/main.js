7f67d7fa8518a821f5eba50d18fc160ef6f186f8
wanglu <376638623@qq.com>
2020-10-18 21:55:14 +0900

    '添加fastclick'

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@ import App from './App.vue'
 import router from './router'
 
 import store from "./store";
+import FastClick from 'fastclick'
 import toast from "./components/common/toast";
 
 Vue.config.productionTip = false
 
 Vue.prototype.$bus = new Vue()
 Vue.use(toast)
+FastClick.attach(document.body)
 new Vue({
   render: h => h(App),
   router,
