1016b95a0469cca4096a4a3b3fc3381033de7c1b
wanglu <376638623@qq.com>
2020-10-19 13:29:43 +0900

    '添加lazyload'

--- src/main.js
+++ src/main.js
@@ -4,12 +4,15 @@ import router from './router'
 
 import store from "./store";
 import FastClick from 'fastclick'
+import VueLazyload from "vue-lazyload";
+
 import toast from "./components/common/toast";
 
 Vue.config.productionTip = false
 
 Vue.prototype.$bus = new Vue()
 Vue.use(toast)
+Vue.use(VueLazyload)
 FastClick.attach(document.body)
 new Vue({
   render: h => h(App),
