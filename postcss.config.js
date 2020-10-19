d5cd27336d9a4e8a17de34bb4fe25f8781d70330
wanglu <376638623@qq.com>
2020-10-19 14:06:41 +0900

    '添加px2vw插件'

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   plugins: {
-    autoprefixer: {}
+    autoprefixer: {},
+    "postcss-px-to-viewport":{
+      viewportWidth: 375,//视图的宽度，对应的是设计稿
+      viewportHeight: 667,//视图高度,对应的是设计稿的高度
+      unitPrecision: 5,//指定'px'转换为视窗单位值的小数位数
+      viewportUnit: 'vw',//指定转换为视窗单位,建议使用vw
+      selectorBlackList: ['ignore'],//指定不需要转换的类
+      minPixelValue: 1,//小于或者等于1px不转换为视窗单位
+      mediaQuery: false,//允许媒体查询中转换为'px'
+    }
   }
 }
